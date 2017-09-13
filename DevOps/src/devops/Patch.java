7ea88caaf856a8d65b7e59efa0fb620bef6b6309
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-09-13 18:26:21 +0300

    Initial commit

---
+++ DevOps/src/devops/Patch.java
@@ -0,0 +1,11 @@
+package devops;
+
+public class Patch {
+    /*  This class patch one file by special command file
+
+    */
+    public static void main(String[] args){
+        System.out.println("File processor ");
+
+    }
+}
