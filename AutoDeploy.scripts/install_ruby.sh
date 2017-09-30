ec963a81d9d23b8b103c1b5551a1951bca975e43
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-09-30 21:55:40 +0300

    correct path to rvm

--- AutoDeploy.scripts/install_ruby.sh
+++ AutoDeploy.scripts/install_ruby.sh
@@ -2,8 +2,8 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
-rvm requirements
-rvm install 2.4.1
-rvm use 2.4.1 --default
+/usr/local/rvm/bin/rvm requirements
+/usr/local/rvm/bin/rvm install 2.4.1
+/usr/local/rvm/bin/rvm use 2.4.1 --default
 /usr/local/rvm/rubies/ruby-2.4.1/bin/gem/gem install bundler -V --no-ri --no-rdoc
 
