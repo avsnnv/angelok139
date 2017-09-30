e2395b478e995eecf440b450fa70e70f04d2a284
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-09-30 21:29:49 +0300

    Autodeploy Script

---
+++ AutoDeploy.scripts/auto_deploy.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+yum install git -y
+git clone https://github.com/angelok139/angelok139
+angelok139/AutoDeploy.scripts/install_ruby.sh
+angelok139/AutoDeploy.scripts/install_mongodb.sh
+angelok139/AutoDeploy.scripts/deploy.sh
+
