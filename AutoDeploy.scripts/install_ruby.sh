7aac8688de65ae359f18fb932cc88fa065ea2e5e
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-09-30 21:32:28 +0300

    small fixes

--- AutoDeploy.scripts/install_ruby.sh
+++ AutoDeploy.scripts/install_ruby.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-~/.rvm/bin/rvm requirements
-~/.rvm/bin/rvm install 2.4.1
-~/.rvm/bin/rvm use 2.4.1 --default
+source /etc/profile.d/rvm.sh
+rvm requirements
+rvm install 2.4.1
+rvm use 2.4.1 --default
 gem install bundler -V --no-ri --no-rdoc
 
