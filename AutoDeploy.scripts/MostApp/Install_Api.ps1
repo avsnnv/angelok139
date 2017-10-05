c84b54d18173d93798cbed8f618e138ee37005d4
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-05 15:05:23 +0300

    Add Install_Services script

--- AutoDeploy.scripts/MostApp/Install_Api.ps1
+++ AutoDeploy.scripts/MostApp/Install_Api.ps1
@@ -58,5 +58,5 @@ Move-Item packages.config $path_api\
 
 Remove-Item  $path_api\Web.config
 Move-Item Web.$Release.config $path_api\
-Rename-Item $path_api\Web.Release.config $path_api\Web.config
+Rename-Item $path_api\Web.$Release.config $path_api\Web.config
 } -ArgumentList $Release,$path_api
---
+++ AutoDeploy.scripts/MostApp/Install_Services.ps1
@@ -0,0 +1,19 @@
+﻿param([String]$server,[String]$Release)
+echo "Install Services on $server release $Release"
+
+Invoke-Command -ComputerName $server -ScriptBlock {
+param([String]$Release)
+
+
+Get-Service -Name Core* | Stop-Service
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installUtil.exe /u C:\CoreServices\bin\Most.Core.API.Services.exe
+
+cd c:\temp\Most.Core\Most.Core.API.Services\bin\$Release
+robocopy . C:\CoreServices\bin /e /XD "References"
+cd References
+robocopy . C:\CoreServices\References /e
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installUtil.exe C:\CoreServices\bin\Most.Core.API.Services.exe
+Get-Service -Name Core* | Start-Service 
+
+} -ArgumentList $Release
