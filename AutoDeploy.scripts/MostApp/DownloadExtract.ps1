790b65c882bd9cd364a9d08c859916719de851ee
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-05 12:09:34 +0300

    Download&Extract script

---
+++ AutoDeploy.scripts/MostApp/DownloadExtract.ps1
@@ -0,0 +1,14 @@
+﻿param([String]$server,[String]$Filename)
+
+echo "Begin Download and Extract $Filename on $server"
+
+Invoke-Command -ComputerName $server -ScriptBlock { param[String] $Filename
+Remove-Item -Recurse -Force c:\temp
+New-Item c:\temp -ItemType Directory
+
+ncftpget -u teamcity -p Qq111111 testserver1.mostapp.com c:\temp\ $Filename
+cd c:\temp\
+
+& "C:\Program Files\7-Zip\7z" x c:\temp\$Filename -oc:\temp\ -y
+
+} -Args $Filename
