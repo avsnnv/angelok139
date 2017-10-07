653683f6c6c0d4a8aa6f793fff43b2709ead3998
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-07 12:05:59 +0300

    Bug fix

--- AutoDeploy.scripts/MostApp/DownloadExtract.ps1
+++ AutoDeploy.scripts/MostApp/DownloadExtract.ps1
@@ -2,7 +2,7 @@
 
 echo "Begin Download and Extract $Filename on $server"
 
-Invoke-Command -ComputerName $server -ScriptBlock { param($Filename)
+Invoke-Command -ComputerName $server -ScriptBlock { param($Filename,$ftpUser,$ftpPass)
 Remove-Item -Recurse -Force c:\temp
 New-Item c:\temp -ItemType Directory
 
@@ -11,4 +11,4 @@ cd c:\temp\
 
 & "C:\Program Files\7-Zip\7z" x c:\temp\$Filename -oc:\temp\ -y
 
-} -Args $Filename
+} -Args $Filename,$ftpUser,$ftpPass
