833569e9cf58d81def288f294c8700a3188f924b
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-11 15:13:30 +0300

    deploy.sh fix

--- AutoDeploy.scripts/deploy.sh
+++ AutoDeploy.scripts/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo $PATH
 export PATH=/usr/local/rvm/rubies/ruby-2.4.1/bin:$PATH
-
+yum install git -y
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit && bundle install
 puma -d
