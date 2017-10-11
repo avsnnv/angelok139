3888cf24d82ebccec2d2b0cafb38fd618f4031f9
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-11 17:57:18 +0300

    AutoDeploy fix

--- AutoDeploy.scripts/deploy.sh
+++ AutoDeploy.scripts/deploy.sh
@@ -2,7 +2,7 @@
 echo $PATH
 export PATH=/usr/local/rvm/rubies/ruby-2.4.1/bin:$PATH
 yum install git -y
+cd /usr/share
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit && bundle install
-puma -d
 
