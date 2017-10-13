fa27fd0605abd34dc38c4e735ea4db96c68bae44
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-13 18:33:13 +0300

    Add input variables to Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,53 +1,57 @@
 provider "google" {
-  project = "infra-182709"
-  region = "asia-northeast1-a"
+  project = "${var.project}"
+  region  = "${var.region}"
   version = "0.1.3"
 }
 
 resource "google_compute_instance" "app" {
-  name		= "reddit-app"
-  machine_type 	= "g1-small"
-  zone		= "asia-northeast1-a"
-  tags = ["reddit-app"]
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "asia-northeast1-a"
+  tags         = ["reddit-app"]
+
   boot_disk {
     initialize_params {
-      image = "reddit-base-1507899498"
+      image = "${var.disk_image}"
     }
   }
+
   network_interface {
-    network = "default"
-    access_config {}
+    network       = "default"
+    access_config = {}
   }
+
   metadata {
-    sshKeys = "avih:${file("~/.ssh/id_rsa.pub")}"
+    sshKeys = "avih:${file("${var.public_key_path}")}"
   }
 
   connection {
+    type        = "ssh"
+    agent       = "false"
     timeout     = "5m"
-    user	= "avih"
-    private_key = "${file("~/.ssh/id_rsa")}"
+    user        = "avih"
+    private_key = "${file("${var.private_key_path}")}"
   }
 
   provisioner "file" {
-    source = "../AutoDeploy.scripts/pumad.service"
+    source      = "../AutoDeploy.scripts/pumad.service"
     destination = "/tmp/pumad.service"
   }
-
 
   provisioner "remote-exec" {
     script = "../AutoDeploy.scripts/auto_deploy.sh"
   }
-
 }
 
-resource "google_compute_firewall" "firewall_puma"{
-  name = "allow-puma-default"
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
   network = "default"
+
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["reddit-app"]
-}
 
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,21 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1-a"
+}
+
+variable public_key_path {
+  description = "Path to ssh pub key"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key_path {
+  description = "Path to ssh priv key"
+}
+
