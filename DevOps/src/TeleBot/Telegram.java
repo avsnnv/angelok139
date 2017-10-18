807677bd7949db1a2c9e7ef4fcee65353b054a70
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-18 11:48:09 +0300

    remove unused libs

--- DevOps/src/TeleBot/Telegram.java
+++ DevOps/src/TeleBot/Telegram.java
@@ -1,6 +1,5 @@
 package TeleBot;
 
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
