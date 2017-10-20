c4de4955b0e80f9820f46f235a81d52ac03c2ada
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-20 14:34:10 +0300

    Add return code control in SendMessage Fix null-size update array error

--- DevOps/src/TeleBot/SendMessage.java
+++ DevOps/src/TeleBot/SendMessage.java
@@ -4,7 +4,12 @@ public class SendMessage {
     public static void main(String[] args){
         Telegram telegram=new Telegram();
         String send=args[1].replace(" ","%20");
-        telegram.sendMessage(args[0],send);
+        Integer code=telegram.sendMessage(args[0],send);
+        if(code.equals(200)) {
 
+            System.out.println("Message sent. Code: " + code.toString());
+        }
+        else
+            throw new RuntimeException("Message not sended. Error code:" + code.toString());
     }
 }
