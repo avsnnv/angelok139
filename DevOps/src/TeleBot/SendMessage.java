abc0b3a950274307d588fe3e63e256df8b7f6e4d
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-20 17:10:42 +0300

    Add new line support in SendMessage Add version command

--- DevOps/src/TeleBot/SendMessage.java
+++ DevOps/src/TeleBot/SendMessage.java
@@ -6,8 +6,7 @@ public class SendMessage {
             throw new RuntimeException("Usage SendMessage chat_id message path_to_config");
         }
         Telegram telegram=new Telegram(args[2]);
-        String send=args[1].replace(" ","%20");
-        Integer code=telegram.sendMessage(args[0],send);
+        Integer code=telegram.sendMessage(args[0],args[1]);
         if(code.equals(200)) {
 
             System.out.println("Message sent. Code: " + code.toString());
