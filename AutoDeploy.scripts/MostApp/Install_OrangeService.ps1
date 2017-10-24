1b0e974294782b442f551ec25902891fb82962d0
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-24 17:32:18 +0300

    Add Orange Service Support

---
+++ AutoDeploy.scripts/MostApp/Install_OrangeService.ps1
@@ -0,0 +1,13 @@
+﻿param([String]$server,[String]$Release,[String]$path_Orange)
+echo "Install API on $server release $Release to $path_Orange"
+
+Invoke-Command -ComputerName $server -ScriptBlock {
+param([String]$Release,[String]$path_Orange)
+
+cd c:\temp\Most.Core\Most.Core.OrangeDataService
+
+robocopy bin $path_Orange
+
+Copy-Item Web.$Release.config $path_Sirena\Web.config
+
+} -ArgumentList $Release,$path_Orange
