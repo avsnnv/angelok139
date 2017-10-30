6115c14ce0675238b2f372237137f9e5658f7df8
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-30 17:49:09 +0300

    Add enviropment variable

--- AutoDeploy.scripts/MostApp/ArcUploadMostappSite.ps1
+++ AutoDeploy.scripts/MostApp/ArcUploadMostappSite.ps1
@@ -1,4 +1,4 @@
-﻿param([String]$Filename,[String]$Release)
+﻿param([String]$Filename,[String]$Release,[String]$Enviropment)
 
 echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
 
@@ -16,5 +16,6 @@ Copy-Item Most.Core\Most.Core.API.Services\NLog.$Release.config Most.Core\Most.C
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core\Most.Core.Statistics.Web
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core\Most.Core.OrangeDataService
 copy $Filename c:\AutoDeploy\ftp_root
-Remove-Item -force c:\AutoDeploy\ftp_root\Most.Core.Last.$Release
-cmd.exe /c mklink c:\AutoDeploy\ftp_root\Most.Core.Last.$Release c:\AutoDeploy\ftp_root\$Filename
+Remove-Item -force c:\AutoDeploy\ftp_root\Most.Core.Last.$Enviropment
+cmd.exe /c mklink c:\AutoDeploy\ftp_root\Most.Core.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
+
