0aff796d913a3861560414a3e5dc2ab5162308d1
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-30 19:02:38 +0300

    Add installing keys

--- AutoDeploy.scripts/MostApp/Install_OrangeService.ps1
+++ AutoDeploy.scripts/MostApp/Install_OrangeService.ps1
@@ -12,6 +12,8 @@ robocopy OrangeService $path_Orange\OrangeService
 
 Copy-Item Web.$Release.config $path_Orange\Web.config
 robocopy Keys.$Release C:\OrangeCert
-& "C:\Program Files\7-Zip\7z" x C:\OrangeCert\client.rar -oc:\OrangeCert\ -y
+& "C:\Program Files\7-Zip\7z" x C:\OrangeCert\7708279602.rar -oc:\OrangeCert\ -y
+$password=ConvertTo-SecureString -String 1234 -AsPlainText -force
+Import-PfxCertificate -Password $password -CertStoreLocation Cert:\LocalMachine\My -FilePath C:\OrangeCert\clientA.pfx
 
 } -ArgumentList $Release,$path_Orange
