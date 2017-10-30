64c2078877e2bdb019d3c779021bcc36cd10691e
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-10-30 17:27:26 +0300

    Remove Symlink before creating

--- AutoDeploy.scripts/MostApp/ArcUploadMostappSite.ps1
+++ AutoDeploy.scripts/MostApp/ArcUploadMostappSite.ps1
@@ -16,4 +16,5 @@ Copy-Item Most.Core\Most.Core.API.Services\NLog.$Release.config Most.Core\Most.C
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core\Most.Core.Statistics.Web
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core\Most.Core.OrangeDataService
 copy $Filename c:\AutoDeploy\ftp_root
-cmd.exe /c mklink Most.Core.Last.$Release c:\AutoDeploy\ftp_root\$Filename
+Remove-Item -force c:\AutoDeploy\ftp_root\Most.Core.Last.$Release
+cmd.exe /c mklink c:\AutoDeploy\ftp_root\Most.Core.Last.$Release c:\AutoDeploy\ftp_root\$Filename
