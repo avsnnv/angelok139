2dff201e946da02280ece598d2078fd5056786de
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-11-03 11:15:42 +0300

    fix comma

--- AutoDeploy.scripts/MostApp/Install_Api.ps1
+++ AutoDeploy.scripts/MostApp/Install_Api.ps1
@@ -1,5 +1,5 @@
 ﻿param([String]$server,[String]$Release,[String]$path_api)
 echo "Install API on $server release $Release to $path_api"
 
-Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_Api.ps1 -ArgumentList $Release $path_api
+Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_Api.ps1 -ArgumentList $Release,$path_api
 
