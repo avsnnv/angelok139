1995911595c09745044f29fb9566adca3b007569
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-11-08 12:49:08 +0300

    fix filename

--- AutoDeploy.scripts/MostApp/ArcUploadBrandService.ps1
+++ AutoDeploy.scripts/MostApp/ArcUploadBrandService.ps1
@@ -3,9 +3,9 @@
 echo "Prepare and Archive Distributive BrandService for $Release enviropment to $Filename"
 
 Remove-Item -force BrandService\Web.config
-Copy-Item BrandService\Web.Debug.config BrandService\Web.config
+Copy-Item BrandService\Web.$Release.config BrandService\Web.config
 
-& "C:\Program Files\7-Zip\7z.exe" a -r $FileName BrandService
-copy $FileName c:\AutoDeploy\ftp_root
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename BrandService
+copy $Filename c:\AutoDeploy\ftp_root
 
-cmd.exe /c mklink c:\AutoDeploy\ftp_root\BrandService.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
+cmd.exe /c mklink c:\AutoDeploy\ftp_root\BrandService.Last.$Release c:\AutoDeploy\ftp_root\$Filename
