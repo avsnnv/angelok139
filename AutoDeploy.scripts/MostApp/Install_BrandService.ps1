0bd548ef57f34cd504362fc99122154776b2cfa7
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-11-08 17:14:13 +0300

    Split install SirenaWCF and BrandService

--- AutoDeploy.scripts/MostApp/Install_BrandService.ps1
+++ AutoDeploy.scripts/MostApp/Install_BrandService.ps1
@@ -1,14 +1,5 @@
 ﻿param([String]$server,[String]$Release,[String]$path_BrandService)
 echo "Install API on $server release $Release to $path_BrandService"
 
-Invoke-Command -ComputerName $server -ScriptBlock {
-param([String]$Release,[String]$path_BrandService)
+Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_BrandService.ps1 -ArgumentList $Release,$path_BrandService
 
-cd c:\temp\BrandService\bin
-robocopy . $path_BrandService\bin /e /XF "Web.config"
-cd ..
-Copy-Item Web.config $path_BrandService -force
-Copy-Item BrandService.svc $path_BrandService -force
-
-
-} -ArgumentList $Release,$path_BrandService
---
+++ AutoDeploy.scripts/MostApp/LocalInstall_SirenaWCF.ps1
@@ -0,0 +1,7 @@
+﻿param([String]$Release,[String]$path_Sirena)
+
+cd c:\temp
+robocopy SirenaTravelProxyGateWcf\bin\$Release $path_Sirena\bin 
+
+Copy-Item SirenaTravelProxyGateWcf\Web.config $path_Sirena
+
