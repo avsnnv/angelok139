4645ded066a06e2c6bcc89bc51707049c32d9a9e
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-11-08 16:00:51 +0300

    Add BrandService Install script

---
+++ AutoDeploy.scripts/MostApp/Install_BrandService.ps1
@@ -0,0 +1,14 @@
+﻿param([String]$server,[String]$Release,[String]$path_BrandService)
+echo "Install API on $server release $Release to $path_BrandService"
+
+Invoke-Command -ComputerName $server -ScriptBlock {
+param([String]$Release,[String]$path_BrandService)
+
+cd c:\temp\BrandService\bin
+robocopy . $path_BrandService\bin /e /XF "Web.config"
+cd ..
+Copy-Item Web.config $path_BrandService -force
+Copy-Item BrandService.svc $path_BrandService -force
+
+
+} -ArgumentList $Release,$path_BrandService
