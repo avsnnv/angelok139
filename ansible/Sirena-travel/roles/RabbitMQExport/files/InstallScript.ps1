acef1d33c52e77b1eae1f6eeca8a8bf196f9ce9d
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-11-28 17:44:52 +0300

    Add role RabbitMQExport

---
+++ ansible/Sirena-travel/roles/RabbitMQExport/files/InstallScript.ps1
@@ -0,0 +1,9 @@
+mkdir c:\temp
+ncftpget -u teamcity -p Qq111111 10.1.8.155 c:\temp\ RMQWorkerExportUT.Last
+& "C:\Program Files\7-Zip\7z" x c:\temp\RMQWorkerExportUT.Last.7z -oc:\temp -y
+
+mkdir c:\projects\RMQWorkerExportUT\
+Copy-Item C:\temp\Build\RMQWorkerExportUT\Release\*.* c:\projects\RMQWorkerExportUT\ -Force
+c:\projects\RMQWorkerExportUT\RMQWorkerExportUT -install name RMQWorkerExportUT
+Start-Service RMQWorkerExportUT
+
