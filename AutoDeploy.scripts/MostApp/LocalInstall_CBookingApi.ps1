cf5a1d170900361e5e33131a945c524f3607adcf
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-06 18:25:30 +0300

    remove folder before extract

--- AutoDeploy.scripts/MostApp/LocalInstall_CBookingApi.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_CBookingApi.ps1
@@ -1,5 +1,5 @@
 ﻿param([String]$Release,[String]$path_CBookingApi)
-
+remove-item $path_CBookingApi -recurse -force
 mkdir $path_CBookingApi
 
 Move-Item c:\temp\Most.Core.CBooking.API\* $path_CBookingApi\
