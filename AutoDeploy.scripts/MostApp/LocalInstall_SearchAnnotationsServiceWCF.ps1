d156cc821fb159b526390a1e34ffc3237f16e504
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-11 13:35:35 +0300

    fix

--- AutoDeploy.scripts/MostApp/LocalInstall_SearchAnnotationsServiceWCF.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_SearchAnnotationsServiceWCF.ps1
@@ -1,8 +1,6 @@
 ﻿param([String]$Release,[String]$path_SearchAnnotationsServiceWCF)
 
-cd c:\temp\SearchAnnotationsServiceWCF\bin
-robocopy . $path_SearchAnnotationsServiceWCF\bin /e /XF "Web.config"
-cd ..
-Copy-Item Web.config $path_SearchAnnotationsServiceWCF -force
-Copy-Item WCFService\SearchAnnotationsService.svc $path_SearchAnnotationsServiceWCF -force
+Remove-Item -force -recurse $path_SearchAnnotationsServiceWCF
+mkdir $path_SearchAnnotationsServiceWCF
+Move-Item c:\temp\SearchAnnotationsServiceWCF\* $path_SearchAnnotationsServiceWCF
 
