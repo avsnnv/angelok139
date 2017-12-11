3f50adeb21dad421b462c73d30606f024fdca4f9
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-11 13:14:02 +0300

    Add SearchAnnotationsServiceWCF

---
+++ AutoDeploy.scripts/MostApp/ArcUploadAnnotationService.ps1
@@ -0,0 +1,13 @@
+﻿param($Filename,[String]$Release)
+
+echo "Prepare and Archive Distributive AnnotationService for $Release enviropment to $Filename"
+
+Remove-Item -force SearchAnnotationsServiceWCF\Web.config
+Copy-Item SearchAnnotationsServiceWCF\Web.$Release.config SearchAnnotationsServiceWCF\Web.config
+
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename SearchAnnotationsServiceWCF
+copy $Filename c:\AutoDeploy\ftp_root
+
+Remove-Item -force c:\AutoDeploy\ftp_root\SearchAnnotationsServiceWCF.Last.$Release
+
+cmd.exe /c mklink c:\AutoDeploy\ftp_root\SearchAnnotationsServiceWCF.Last.$Release c:\AutoDeploy\ftp_root\$Filename
---
+++ AutoDeploy.scripts/MostApp/Install_SearchAnnotationsServiceWCF.ps1
@@ -0,0 +1,5 @@
+﻿param([String]$server,[String]$Release,[String]$path_SearchAnnotationsServiceWCF)
+echo "Install SearchAnnotationsServiceWCF on $server release $Release to $path_BrandService"
+
+Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_SearchAnnotationsServiceWCF.ps1 -ArgumentList $Release,$path_SearchAnnotationsServiceWCF
+
