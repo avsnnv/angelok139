0819220a580a7e94507484a8b349511ffd6c80d3
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-11 15:10:29 +0300

    fix name

--- AutoDeploy.scripts/MostApp/Install_ElmaWCF.ps1
+++ AutoDeploy.scripts/MostApp/Install_ElmaWCF.ps1
@@ -1,5 +1,5 @@
 ﻿param([String]$server,[String]$Release,[String]$path_WcfElma)
 echo "Install ElmaWCF on $server release $Release to $path_WcfElma"
 
-Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_WcfElma.ps1 -ArgumentList $Release,$path_WcfElma
+Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_ElmaWCF.ps1 -ArgumentList $Release,$path_WcfElma
 
