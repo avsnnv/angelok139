ecd86da16484b76ceea55fb328a585e049d88efe
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-18 18:22:13 +0300

    Add CBooking Upload Script

---
+++ AutoDeploy.scripts/CBooking/ArcUploadCBookingSite.ps1
@@ -0,0 +1,9 @@
+﻿param([String]$Filename,[String]$Release,[String]$Enviropment)
+
+echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
+
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename CbookingWebsite
+copy $Filename c:\AutoDeploy\ftp_root
+Remove-Item -force c:\AutoDeploy\ftp_root\CBooking.Last.$Enviropment
+cmd.exe /c mklink c:\AutoDeploy\ftp_root\CBooking.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
+
