05d8f36b80183a257ad808722e6ac677df49cbaa
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-20 17:47:44 +0300

    Add services to Artifact Delete Download and Extract (moved to ansible role)

--- AutoDeploy.scripts/CBooking/ArcUploadCBookingSite.ps1
+++ AutoDeploy.scripts/CBooking/ArcUploadCBookingSite.ps1
@@ -3,6 +3,8 @@
 echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
 
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename CbookingWebsite
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename hdswebsite hdscontent hdssvc hdsbo hdsgateway buhdocs hdsorderfo hdsorderfo2 channelmanagerapi hotelgateway cmxmlapi 
+
 copy $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\CBooking.Last.$Enviropment
 cmd.exe /c mklink c:\AutoDeploy\ftp_root\CBooking.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
--- AutoDeploy.scripts/CBooking/DownloadExtract.ps1
+++
@@ -1,6 +0,0 @@
-﻿param([String]$server,[String]$Filename,[String]$ftpUser,[String]$ftpPass)
-
-echo "Begin Download and Extract $Filename on $server"
-
-Invoke-Command -ComputerName $server -FilePath Scripts/LocalDownloadExtract.ps1 -ArgumentList $Filename,$ftpUser,$ftpPass
-
