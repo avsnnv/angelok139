1b7b6a5f18f5c9755fdc494b939d974cec914922
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-21 14:17:45 +0300

    fix services deletion

--- AutoDeploy.scripts/MostApp/LocalInstall_Taxi.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_Taxi.ps1
@@ -5,7 +5,7 @@ Remove-Item -force -Recurse $path_TaxiApi
 mkdir $path_TaxiApi
 Move-Item -Force Most.TaxiCore.Api\* $path_TaxiApi
 
-Remove-Item -force -Recurse $path_Services
-mkdir $path_Services
+Remove-Item -force -Recurse $path_Services\GettTaxiWCF
+mkdir $path_Services\GettTaxiWCF
 Move-Item -force Most.TaxiCore.GettService\* $path_Services\GettTaxiWCF
 
