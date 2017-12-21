ff2f8d90534f9ccf84dc82ad7687ffdf24fcaff0
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-21 12:59:56 +0300

    fix deploy script

--- AutoDeploy.scripts/MostApp/LocalInstall_Taxi.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_Taxi.ps1
@@ -1,11 +1,11 @@
 param([String]$path_TaxiApi,[String]$path_Services)
 
 cd c:\temp
-Remove-Item -force $path_TaxiApi
+Remove-Item -force -Recurse $path_TaxiApi
 mkdir $path_TaxiApi
-Move-Item -Recurse Most.TaxiCore.Api\* $path_TaxiApi
+Move-Item -Force Most.TaxiCore.Api\* $path_TaxiApi
 
-Remove-Item -force $path_Services
+Remove-Item -force -Recurse $path_Services
 mkdir $path_Services
-Move-Item -Recurse Most.TaxiCore.GettService\* $path_Services
+Move-Item -force Most.TaxiCore.GettService\* $path_Services
 
