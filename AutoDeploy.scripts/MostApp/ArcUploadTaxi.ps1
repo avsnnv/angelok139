8b4a74e6e48421f9a9de750b515419f3b7b433bf
Alexander Vykhrishchuk <angelok139@gmail.com>
2017-12-21 12:23:19 +0300

    Add Taxi Upload Script

---
+++ AutoDeploy.scripts/MostApp/ArcUploadTaxi.ps1
@@ -0,0 +1,18 @@
+﻿param($Filename,[String]$Release)
+
+echo "Prepare and Archive Distributive CbookingAPI for $Release enviropment to $Filename"
+
+Remove-Item -force Most.TaxiCore.Api\Web.config
+Copy-Item Most.TaxiCore.Api\Web.$Release.config Most.TaxiCore.Api\Web.config
+Remove-Item -force Most.TaxiCore.GettService\Web.config
+Copy-Item Most.TaxiCore.GettService\Web.$Release.config Most.TaxiCore.GettService\Web.config
+
+
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.TaxiCore.Api
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.TaxiCore.GettService
+
+copy $Filename c:\AutoDeploy\ftp_root
+
+Remove-Item -force c:\AutoDeploy\ftp_root\Most.Core.Taxi.API.Last.$Release
+
+cmd.exe /c mklink c:\AutoDeploy\ftp_root\Most.Core.Taxi.API.Last.$Release c:\AutoDeploy\ftp_root\$Filename
