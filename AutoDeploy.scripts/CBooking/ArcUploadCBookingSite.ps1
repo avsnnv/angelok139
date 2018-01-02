69c3a6e4aa037e99150c1c3445171a3c08c89f00
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-01-02 15:50:36 +0300

    rename hdsorerfo

--- AutoDeploy.scripts/CBooking/ArcUploadCBookingSite.ps1
+++ AutoDeploy.scripts/CBooking/ArcUploadCBookingSite.ps1
@@ -3,6 +3,8 @@
 echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
 
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename CbookingWebsite
+rename hdsorderfo hdsorderfo2
+rename hdsorderfo_old hdsorderfo
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename hdswebsite hdscontent hdssvc hdsbo hdsgateway buhdocs hdsorderfo hdsorderfo_old hdsorderfo2 channelmanagerapi hotelgateway cmxmlapi download  
 
 copy $Filename c:\AutoDeploy\ftp_root
