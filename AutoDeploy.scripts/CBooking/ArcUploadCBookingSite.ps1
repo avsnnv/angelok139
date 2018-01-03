24e278e727448af04a0c07d568c11adbca545061
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-01-03 18:10:29 +0300

    Add backoffice & ast.cbooking.com to archive

--- AutoDeploy.scripts/CBooking/ArcUploadCBookingSite.ps1
+++ AutoDeploy.scripts/CBooking/ArcUploadCBookingSite.ps1
@@ -5,7 +5,7 @@ echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename CbookingWebsite
 Move-Item hdsorderfo hdsorderfo2
 Move-Item hdsorderfo_old hdsorderfo
-& "C:\Program Files\7-Zip\7z.exe" a -r $Filename hdswebsite hdscontent hdssvc hdsbo hdsgateway buhdocs hdsorderfo hdsorderfo_old hdsorderfo2 channelmanagerapi hotelgateway cmxmlapi download  
+& "C:\Program Files\7-Zip\7z.exe" a $Filename hdswebsite hdscontent hdssvc hdsbo hdsgateway buhdocs hdsorderfo hdsorderfo2 channelmanagerapi hotelgateway cmxmlapi download Style Backoffice DBExporterConsole VBRunningServiceSQL MailsheduleChecker
 
 copy $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\CBooking.Last.$Enviropment
