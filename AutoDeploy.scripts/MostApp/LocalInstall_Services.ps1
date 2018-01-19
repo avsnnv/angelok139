3f87d8762b197d3a64eb9b0c246f1a9e08f18cce
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-01-19 15:44:04 +0300

    Change stop and start services commands

--- AutoDeploy.scripts/MostApp/LocalInstall_Services.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_Services.ps1
@@ -1,7 +1,14 @@
 param([String]$Release)
 
-
-Get-Service -Name Core* | Stop-Service
+Stop-Service CoreGenerateTicketService
+Stop-Service CoreDepositAirticketsService
+Stop-Service CoreUpdateTrackerService
+Stop-Service CoreUpdateTopTripsService
+Stop-Service CoreParseWatchesToBrandService
+Stop-Service CoreCancelBookingExpiringService
+Stop-Service CoreRemoveTokensExperedService
+Stop-Service CoreDeleteOldLogsService
+Stop-Service CoreDeleteOldWatchService
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installUtil.exe /u C:\CoreServices\bin\Most.Core.API.Services.exe
 
@@ -10,6 +17,14 @@ robocopy . C:\CoreServices\bin /e /XD "References"
 cd References
 robocopy . C:\CoreServices\References /e
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installUtil.exe C:\CoreServices\bin\Most.Core.API.Services.exe
-Get-Service -Name Core* | Start-Service
 
+Start-Service CoreGenerateTicketService
+Start-Service CoreDepositAirticketsService
+Start-Service CoreUpdateTrackerService
+Start-Service CoreUpdateTopTripsService
+Start-Service CoreParseWatchesToBrandService
+Start-Service CoreCancelBookingExpiringService
+Start-Service CoreRemoveTokensExperedService
+Start-Service CoreDeleteOldLogsService
+Start-Service CoreDeleteOldWatchService
 
--- AutoDeploy.scripts/MostApp/StopServices.ps1
+++ AutoDeploy.scripts/MostApp/StopServices.ps1
@@ -1,6 +1,14 @@
 ﻿param([String]$server)
 echo "Begin stopping Services on $server"
 
-Invoke-Command -ComputerName $server -ScriptBlock  {
-Get-Service -Name Core* | Stop-Service 
+Invoke-Command -ComputerName $server -ScriptBlock  {\
+Stop-Service CoreGenerateTicketService
+Stop-Service CoreDepositAirticketsService
+Stop-Service CoreUpdateTrackerService
+Stop-Service CoreUpdateTopTripsService
+Stop-Service CoreParseWatchesToBrandService
+Stop-Service CoreCancelBookingExpiringService
+Stop-Service CoreRemoveTokensExperedService
+Stop-Service CoreDeleteOldLogsService
+Stop-Service CoreDeleteOldWatchService
 }
