697c82320af8c0bf88ab8bfb17ac44c50ba6e37b
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-02-07 17:29:22 +0300

    bug fix

--- AutoDeploy.scripts/MostApp/LocalInstall_MetaApi.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_MetaApi.ps1
@@ -1,6 +1,6 @@
 ﻿param([String]$Release,[String]$path_MetaApi)
 
-cd c:\temp\Most.Core.Meta.API
+cd C:\temp\Most.Core.IntegrationCore\Most.Core.Meta.API
 Remove-Item -force $path_MetaApi
 mkdir $path_MetaApi
 robocopy . $path_MetaApi /E 
