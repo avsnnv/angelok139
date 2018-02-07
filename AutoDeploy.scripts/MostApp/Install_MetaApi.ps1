52edf7a205317b5bb51bc9757e8fd582357f7960
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-02-07 17:19:50 +0300

    Add Install_MetaApi script

---
+++ AutoDeploy.scripts/MostApp/Install_MetaApi.ps1
@@ -0,0 +1,5 @@
+﻿param([String]$server,[String]$Release,[String]$path_MetaApi)
+echo "Install MetaAPI on $server release $Release to $path_MetaApi"
+
+Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_MetaApi.ps1 -ArgumentList $Release,$path_MetaApi
+
---
+++ AutoDeploy.scripts/MostApp/LocalInstall_MetaApi.ps1
@@ -0,0 +1,7 @@
+﻿param([String]$Release,[String]$path_MetaApi)
+
+cd c:\temp\Most.Core.Meta.API
+Remove-Item -force $path_MetaApi
+mkdir $path_MetaApi
+robocopy . $path_MetaApi /E 
+
