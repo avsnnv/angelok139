6c096810052526ba8091b28098d1171bebdf1fba
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-02-13 16:29:56 +0300

    Add install wcf service

--- AutoDeploy.scripts/MostApp/ArcUploadMetaApi.ps1
+++ AutoDeploy.scripts/MostApp/ArcUploadMetaApi.ps1
@@ -5,7 +5,12 @@ echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
 Remove-Item -force Most.Core.IntegrationCore\Most.Core.Meta.API\Web.config
 Copy-Item Most.Core.IntegrationCore\Most.Core.Meta.API\Web.$Release.config Most.Core.IntegrationCore\Most.Core.Meta.API\Web.config
 
+Remove-Item -force Most.Core.IntegrationCore\Most.Core.Meta.WCF\Web.config
+Copy-Item Most.Core.IntegrationCore\Most.Core.Meta.WCF\Web.$Release.config Most.Core.IntegrationCore\Most.Core.Meta.WCF\Web.config
+
+
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core.IntegrationCore\Most.Core.Meta.API
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core.IntegrationCore\Most.Core.Meta.WCF
 copy $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\Most.Core.Meta.Api.$Enviropment
 cmd.exe /c mklink c:\AutoDeploy\ftp_root\Most.Core.Meta.Api.$Enviropment c:\AutoDeploy\ftp_root\$Filename
--- AutoDeploy.scripts/MostApp/Install_MetaApi.ps1
+++ AutoDeploy.scripts/MostApp/Install_MetaApi.ps1
@@ -1,5 +1,5 @@
-﻿param([String]$server,[String]$Release,[String]$path_MetaApi)
-echo "Install MetaAPI on $server release $Release to $path_MetaApi"
+﻿param([String]$server,[String]$Release,[String]$path_MetaApi,[String]$path_MetaWCF)
+echo "Install MetaAPI on $server release $Release to $path_MetaApi and $path_MetaWCF"
 
-Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_MetaApi.ps1 -ArgumentList $Release,$path_MetaApi
+Invoke-Command -ComputerName $server -FilePath Scripts/LocalInstall_MetaApi.ps1 -ArgumentList $Release,$path_MetaApi,$path_MetaWCF
 
