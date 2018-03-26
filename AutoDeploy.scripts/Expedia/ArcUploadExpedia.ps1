1d79eda33fa53ab3f157f3be13b22d05927d7803
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-03-26 11:47:38 +0300

    Arc and upload

---
+++ AutoDeploy.scripts/Expedia/ArcUploadExpedia.ps1
@@ -0,0 +1,10 @@
+﻿param([String]$Filename,[String]$Release,[String]$Enviropment)
+
+echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
+
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Api
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Node
+copy $Filename c:\AutoDeploy\ftp_root
+Remove-Item -force c:\AutoDeploy\ftp_root\Expedia.Last.$Enviropment
+cmd.exe /c mklink c:\AutoDeploy\ftp_root\Expedia.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
+
---
+++ AutoDeploy.scripts/Expedia/DownloadExtract.ps1
@@ -0,0 +1,6 @@
+﻿param([String]$server,[String]$Filename,[String]$ftpUser,[String]$ftpPass)
+
+echo "Begin Download and Extract $Filename on $server"
+
+Invoke-Command -ComputerName $server -FilePath Scripts/LocalDownloadExtract.ps1 -ArgumentList $Filename,$ftpUser,$ftpPass
+
