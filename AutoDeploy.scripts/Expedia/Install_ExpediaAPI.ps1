7a75eed417a37853baa94786d4813e2fb33fb2fb
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-03-26 12:12:57 +0300

    Install API

---
+++ AutoDeploy.scripts/Expedia/Install_ExpediaAPI.ps1
@@ -0,0 +1,5 @@
+﻿param([String]$server,[String]$Release,[String]$path_Sirena)
+echo "Install API on $server release $Release to $path_Sirena"
+
+Invoke-Command -ComputerName $server -FilePath LocalInstall_ExpediaAPI.ps1 -ArgumentList $Release,$path_Sirena
+
---
+++ AutoDeploy.scripts/Expedia/LocalInstall_ExpediaAPI.ps1
@@ -0,0 +1,7 @@
+﻿param([String]$Release,[String]$path_ExpediaAPI)
+
+cd c:\temp
+mkdir $path_Expedia
+robocopy Expedia.Core.Api $path_Expedia\ 
+
+
