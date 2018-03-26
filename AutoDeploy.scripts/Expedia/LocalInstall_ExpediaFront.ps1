227b1f6e721a1bf41ddfe582ac098e41f73b48c8
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-03-26 14:02:45 +0300

    Install frontend

---
+++ AutoDeploy.scripts/Expedia/LocalInstall_ExpediaFront.ps1
@@ -0,0 +1,9 @@
+﻿param([String]$Release,[String]$path_ExpediaFront)
+
+cd c:\temp
+mkdir $path_ExpediaFront
+robocopy Expedia.Core.Node $path_ExpediaFront /E
+cd $path_ExpediaFront 
+npm i
+npm run build:beta
+
