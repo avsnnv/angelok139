7fe67548a2bbf95026c0fb0a40663a21ccf9c3fd
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-03-26 14:15:34 +0300

    fix

--- AutoDeploy.scripts/Expedia/LocalInstall_ExpediaFront.ps1
+++ AutoDeploy.scripts/Expedia/LocalInstall_ExpediaFront.ps1
@@ -4,6 +4,6 @@ cd c:\temp
 mkdir $path_ExpediaFront
 robocopy Expedia.Core.Node $path_ExpediaFront /E
 cd $path_ExpediaFront 
-C:\Program Files\nodejs\npm i
-C:\Program Files\nodejs\npm run build:beta
+& "C:\Program Files\nodejs\npm" i
+& "C:\Program Files\nodejs\npm" run build:beta
 
