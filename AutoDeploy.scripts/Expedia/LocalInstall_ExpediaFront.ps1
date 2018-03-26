2dbe1a81a80867703b46261bce899810ee87453b
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-03-26 16:49:03 +0300

    Remove npm

--- AutoDeploy.scripts/Expedia/LocalInstall_ExpediaFront.ps1
+++ AutoDeploy.scripts/Expedia/LocalInstall_ExpediaFront.ps1
@@ -4,6 +4,4 @@ cd c:\temp
 mkdir $path_ExpediaFront
 robocopy Expedia.Core.Node $path_ExpediaFront /E
 cd $path_ExpediaFront 
-& "C:\Program Files\nodejs\npm" i
-& "C:\Program Files\nodejs\npm" run build:beta
 
