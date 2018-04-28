be37329112b91e613df14af5823513dca9684243
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-04-28 16:26:19 +0300

    Add Install Expedia Services

--- AutoDeploy.scripts/Expedia/ArcUploadExpedia.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpedia.ps1
@@ -9,6 +9,13 @@ Remove-Item -force Web.config
 Copy-Item Web.$Release.config Web.config
 cd ..
 
+Remove-Item -force Expedia.Core.Services\bin\$Release\Expredia.Core.WinServices.exe.config
+Copy-Item Expedia.Core.Services\App.$Release.config Expedia.Core.Services\bin\$Release\Expredia.Core.WinServices.exe.config
+
+Remove-Item -force Expedia.Core.Services\bin\$Release\NLog.config
+Copy-Item Expedia.Core.Services\NLog.$Release.config Expedia.Core.Services\bin\$Release\NLog.config
+
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Services
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Api
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Node
 copy $Filename c:\AutoDeploy\ftp_root
