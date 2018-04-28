f689d4001429c8610ef17b2fa7a24cd63bebf8f4
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-04-28 23:03:44 +0300

    Correct another filename

--- AutoDeploy.scripts/Expedia/LocalInstall_Services.ps1
+++ AutoDeploy.scripts/Expedia/LocalInstall_Services.ps1
@@ -4,11 +4,11 @@ Stop-Service ExpediaDepositOrderService
 Stop-Service ExpediaCancelExpiringCartService
 Stop-Service ExpediaUpdateCartStatusService
 
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installUtil.exe /u c:\ExpediaServices\bin\Expredia.Core.WinServices.exe
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installUtil.exe /u c:\ExpediaServices\bin\Expedia.Core.WinServices.exe
 
 cd c:\temp\Expredia.Core.WinServices\bin\$Release
 Remove-Item -force -recurse c:\ExpediaServices
 mkdir c:\ExpediaServices\bin
 robocopy . C:\ExpediaServices\bin /e
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installUtil.exe c:\ExpediaServices\bin\Expredia.Core.WinServices.exe
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installUtil.exe c:\ExpediaServices\bin\Expedia.Core.WinServices.exe
 
