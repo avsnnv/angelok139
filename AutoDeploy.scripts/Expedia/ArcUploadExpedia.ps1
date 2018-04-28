136ce17e768a8c5006d2f8f61348146834fa1f78
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-04-28 16:37:09 +0300

    change copy to move

--- AutoDeploy.scripts/Expedia/ArcUploadExpedia.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpedia.ps1
@@ -18,7 +18,7 @@ Copy-Item Expedia.Core.Services\NLog.$Release.config Expedia.Core.Services\bin\$
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Services
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Api
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Node
-copy $Filename c:\AutoDeploy\ftp_root
+Move-Item $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\Expedia.Last.$Enviropment
 cmd.exe /c mklink c:\AutoDeploy\ftp_root\Expedia.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
 
