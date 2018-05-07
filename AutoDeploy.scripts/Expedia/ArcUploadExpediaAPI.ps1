a2c7fbf3376eb3700f6405940d3e4054b5a24d06
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-05-07 14:47:52 +0300

    fix

--- AutoDeploy.scripts/Expedia/ArcUploadExpediaAPI.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpediaAPI.ps1
@@ -5,8 +5,7 @@ cd Expedia.Core.Api
 Remove-Item -force Web.config
 Copy-Item Web.$Release.config Web.config
 cd ..
-
-Compress-Archive -Path Expedia.Core.Api\ -destinationPath $Filename -CompressionLevel Optimal
+& "C:\Program Files\7-Zip\7z.exe" a -tzip -r $Filename Expedia.Core.Api
 Move-Item $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\Expedia.Api.Last.$Enviropment
 cmd.exe /c mklink c:\AutoDeploy\ftp_root\Expedia.Api.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
