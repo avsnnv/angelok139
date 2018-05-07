01b858c38b382dc06e3a05dd54ced955a16f736a
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-05-07 14:41:53 +0300

    Transfer to zip

--- AutoDeploy.scripts/Expedia/ArcUploadExpediaAPI.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpediaAPI.ps1
@@ -6,8 +6,7 @@ Remove-Item -force Web.config
 Copy-Item Web.$Release.config Web.config
 cd ..
 
-& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expredia.Core.WinServices
-& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Api
+Compress-Archive -Path Expedia.Core.Api\ -destinationPath $Filename -CompressionLevel Optimal
 Move-Item $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\Expedia.Api.Last.$Enviropment
 cmd.exe /c mklink c:\AutoDeploy\ftp_root\Expedia.Api.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
