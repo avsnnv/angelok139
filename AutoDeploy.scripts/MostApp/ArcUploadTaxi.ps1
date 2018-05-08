96178f971e6a268105da7d57774bae753fa1f073
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-05-08 14:27:05 +0300

    add YandexTaxi support

--- AutoDeploy.scripts/MostApp/ArcUploadTaxi.ps1
+++ AutoDeploy.scripts/MostApp/ArcUploadTaxi.ps1
@@ -6,10 +6,12 @@ Remove-Item -force Most.TaxiCore.Api\Web.config
 Copy-Item Most.TaxiCore.Api\Web.$Release.config Most.TaxiCore.Api\Web.config
 Remove-Item -force Most.TaxiCore.GettService\Web.config
 Copy-Item Most.TaxiCore.GettService\Web.$Release.config Most.TaxiCore.GettService\Web.config
-
+Remove-Item -force Most.TaxiCore.YandexService\Web.config
+Copy-Item Most.TaxiCore.GettService\Web.$Release.config Most.TaxiCore.YandexService\Web.config
 
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.TaxiCore.Api
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.TaxiCore.GettService
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.TaxiCore.YandexService
 
 copy $Filename c:\AutoDeploy\ftp_root
 
