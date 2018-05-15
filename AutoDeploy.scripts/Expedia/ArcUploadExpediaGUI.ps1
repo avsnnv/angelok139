4f2037e8d35e0781305a72bb9881d6f8cbc22f0d
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-05-15 13:24:02 +0300

    Move npm to testserver

--- AutoDeploy.scripts/Expedia/ArcUploadExpediaGUI.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpediaGUI.ps1
@@ -1,6 +1,10 @@
-﻿param([String]$Filename,[String]$Release,[String]$Enviropment)
+﻿param([String]$Filename,[String]$Release,[String]$Enviropment,$runCommand)
 
 echo "Prepare and Archive Expedia GUI for $Release enviropment to $Filename"
+cd Expedia.Core.Node
+npm install -f
+npm run build:$runCommand
+cd ..
 & "C:\Program Files\7-Zip\7z.exe" a -tzip -r $Filename Expedia.Core.Node
 Move-Item $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\Expedia.Node.Last.$Enviropment
--- AutoDeploy.scripts/Expedia/Install_ExpediaFront.ps1
+++ AutoDeploy.scripts/Expedia/Install_ExpediaFront.ps1
@@ -1,5 +1,5 @@
-﻿param([String]$server,[String]$Release,[String]$path_ExpediaFront)
+﻿param([String]$server,[String]$Release,[String]$path_ExpediaFront,$runCommand)
 echo "Install API on $server release $Release to $path_ExpediaFront"
 
-Invoke-Command -ComputerName $server -FilePath LocalInstall_ExpediaFront.ps1 -ArgumentList $Release,$path_ExpediaFront
+Invoke-Command -ComputerName $server -FilePath LocalInstall_ExpediaFront.ps1 -ArgumentList $Release,$path_ExpediaFront,$runCommand
 
