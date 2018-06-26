1de59386c711f8cead870e2a4e24d3ef9318041e
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-06-26 18:19:43 +0300

    Add parametrization for CBookingAPI Add Service Install for CBookingAPI

--- AutoDeploy.scripts/MostApp/ArcUploadCbookingAPI.ps1
+++ AutoDeploy.scripts/MostApp/ArcUploadCbookingAPI.ps1
@@ -5,7 +5,16 @@ echo "Prepare and Archive Distributive CbookingAPI for $Release enviropment to $
 Remove-Item -force Most.Core.CBooking.API\Web.config
 Copy-Item Most.Core.CBooking.API\Web.$Release.config Most.Core.CBooking.API\Web.config
 
+Remove-Item -force Most.Core.CBooking.WinServices\bin\$Release\Most.Core.CBooking.WinServices.exe.config
+Copy-Item Most.Core.CBooking.WinServices\App.$Release.config Most.Core.CBooking.WinServices\bin\$Release\Most.Core.CBooking.WinServices.exe.config
+
+Remove-Item -force Most.Core.CBooking.WinServices\bin\$Release\NLog.config
+Copy-Item Most.Core.CBooking.WinServices\NLog.$Release.config Most.Core.CBooking.WinServices\bin\$Release\NLog.config
+
+
+
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core.CBooking.API\
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core.CBooking.WinServices\
 copy $Filename c:\AutoDeploy\ftp_root
 
 Remove-Item -force c:\AutoDeploy\ftp_root\Most.Core.CBooking.API.Last.$Release
