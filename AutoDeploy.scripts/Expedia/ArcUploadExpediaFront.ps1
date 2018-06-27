aeae28e58a3b9a212df0ed4d47cd0e2847b200c7
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-06-27 11:55:14 +0300

    Revert "Remove path"
    This reverts commit 7add06a997480986a5ffeaeaec070c53ebfa18b3.

--- AutoDeploy.scripts/Expedia/ArcUploadExpediaFront.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpediaFront.ps1
@@ -1,8 +1,10 @@
 param([String]$Filename,[String]$Release,[String]$Enviropment,[String]$runCommand)
 echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
+cd Expedia.Core.Node
 npm install -f
 npm run build:$runCommand
-& "C:\Program Files\7-Zip\7z.exe" a -r $Filename *
+cd ..
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Node
 Move-Item $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\Expedia.Front.Last.$Enviropment
 cmd.exe /c mklink c:\AutoDeploy\ftp_root\Expedia.Front.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
