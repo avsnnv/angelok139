28975a6ceb74676ea6cd4fe71101363894ed14fd
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-06-27 11:48:04 +0300

    Correct path

--- AutoDeploy.scripts/Expedia/ArcUploadExpediaFront.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpediaFront.ps1
@@ -2,8 +2,7 @@ param([String]$Filename,[String]$Release,[String]$Enviropment,[String]$runComman
 echo "Prepare and Archive Distributive for $Release enviropment to $Filename"
 npm install -f
 npm run build:$runCommand
-cd ..
-& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Node
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename *
 Move-Item $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\Expedia.Front.Last.$Enviropment
 cmd.exe /c mklink c:\AutoDeploy\ftp_root\Expedia.Front.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
