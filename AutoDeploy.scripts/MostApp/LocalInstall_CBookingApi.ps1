a518e167c3b0fe9dfa282cccdd750e9b84b43d3e
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-06-29 17:19:33 +0300

    add force

--- AutoDeploy.scripts/MostApp/LocalInstall_CBookingApi.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_CBookingApi.ps1
@@ -4,7 +4,7 @@ mkdir $path_CBookingApi
 
 Move-Item c:\temp\Most.Core.CBooking.API\* $path_CBookingApi\
 
-remove-item $path_services\CbookingSettingsService
+remove-item $path_services\CbookingSettingsService -recurse -force
 mkdir $path_services\CbookingSettingsService
 Move-Item c:\temp\Most.Core.SettingsService\* $path_services\CbookingSettingsService\
 
