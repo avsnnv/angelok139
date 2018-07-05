e6c76fb1637ff30cbb67fdd0221acfff1bad1e93
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-07-05 11:50:41 +0300

    Add zabbix vars

--- AutoDeploy.scripts/Expedia/LocalInstall_ExpediaAPI.ps1
+++ AutoDeploy.scripts/Expedia/LocalInstall_ExpediaAPI.ps1
@@ -1,6 +1,13 @@
 ﻿param([String]$Release,[String]$path_ExpediaAPI)
 
 cd c:\temp
+
+git clone https://github.com/angelok139/angelok139
+cd angelok139
+javac DevOps\src\devops\*
+java -cp .\DevOps\src devops.Patch --inputfile=..\Expedia.Core.Api\Web.config --outputfile=..\Most.Core\Most.Core.API\Web.config --patchfile=.\Patches\testserver.patch
+java -cp .\DevOps\src devops.Patch --inputfile=..\Expedia.Core.WinServices\App.$Release.config --outputfile=..\Most.Core\Most.Core.API\Web.config --patchfile=.\Patches\testserver.patch
+cd c:\temp
 mkdir $path_ExpediaAPI
 robocopy Expedia.Core.Api $path_ExpediaAPI /E 
 
