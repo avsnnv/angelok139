cb2343826c965161e5bd736e459a9916345517a2
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-07-16 13:45:57 +0300

    Add Orange Arch

--- AutoDeploy.scripts/Expedia/ArcUploadExpedia.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpedia.ps1
@@ -20,6 +20,7 @@ Copy-Item Expedia.Core.WinServices\NLog.$Release.config Expedia.Core.WinServices
 
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.WinServices
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Api
+& "C:\Program Files\7-Zip\7z.exe" a -r $Filename Most.Core.OrangeDataService
 Move-Item $Filename c:\AutoDeploy\ftp_root
 Remove-Item -force c:\AutoDeploy\ftp_root\Expedia.API.Last.$Enviropment
 cmd.exe /c mklink c:\AutoDeploy\ftp_root\Expedia.API.Last.$Enviropment c:\AutoDeploy\ftp_root\$Filename
