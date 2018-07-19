aaef5cad5746aa581d3062efc217548fe0d62987
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-07-19 16:10:58 +0300

    fix

--- AutoDeploy.scripts/MostApp/LocalInstall_Api.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_Api.ps1
@@ -4,6 +4,4 @@ cd c:\temp\Most.Core\
 $hostname = (hostname| Out-String).Replace("`r`n","")
 (Get-Content Most.Core.API\Web.$Release.config).replace('$HostName',$hostname)|Set-Content Most.Core.API\Web.config
 
-Remove-Item $path_api -Recurse 
-New-Item $path_api -ItemType Directory
 robocopy Most.Core.API $path_api /E
