214a3c3c5bcb7b1df863e28a9c8f264024b8c2a3
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-07-19 17:44:35 +0300

    Return powershell

--- AutoDeploy.scripts/MostApp/LocalInstall_Api.ps1
+++ AutoDeploy.scripts/MostApp/LocalInstall_Api.ps1
@@ -1,10 +1,6 @@
 param([String]$Release,[String]$path_api)
 
-cd c:\temp
-git clone https://github.com/angelok139/angelok139
-cd angelok139
-javac DevOps\src\devops\*
-java -cp .\DevOps\src devops.Patch --inputfile=..\Most.Core\Most.Core.API\Web.$Release.config --outputfile=..\Most.Core\Most.Core.API\Web.config --patchfile=.\Patches\testserver.patch
-
 cd c:\temp\Most.Core\
+$hostname = (hostname| Out-String).Replace("`r`n","")
+(Get-Content Most.Core.API\Web.$Release.config -Encoding UTF8).replace('$HostName',$hostname)|Set-Content Most.Core.API\Web.config -Encoding UTF8
 robocopy Most.Core.API $path_api /E
