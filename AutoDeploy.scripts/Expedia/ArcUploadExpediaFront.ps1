204916951740a1f7297cf3935caf66c2dfb7f32e
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-07-20 12:02:04 +0300

    I.lowercase()

--- AutoDeploy.scripts/Expedia/ArcUploadExpediaFront.ps1
+++ AutoDeploy.scripts/Expedia/ArcUploadExpediaFront.ps1
@@ -4,7 +4,7 @@ cd Expedia.Core.Node
 npm install -f
 npm run build:$runCommand
 Remove-Item -force -Recurse node_modules
-npm I --production
+npm i --production
 cd ..
 & "C:\Program Files\7-Zip\7z.exe" a -r $Filename Expedia.Core.Node
 Move-Item $Filename c:\AutoDeploy\ftp_root
