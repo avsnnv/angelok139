a430e9f790ca01880f9430d1e18a8ec395c63d40
Alexander Vykhrishchuk <angelok139@gmail.com>
2018-11-29 13:16:18 +0300

    bug fix

--- AutoDeploy.scripts/Expedia/Install_OutputService.ps1
+++ AutoDeploy.scripts/Expedia/Install_OutputService.ps1
@@ -1,5 +1,5 @@
 ﻿param([String]$server,[String]$Release,[String]$path_OutputService)
 echo "Install API on $server release $Release to $path_OutputService"
 
-Invoke-Command -ComputerName $server -FilePath LocalInstall_OutputService.ps1 -ArgumentList $Release,$path_OutputService
+Invoke-Command -ComputerName $server -FilePath ScriptsExpedia\LocalInstall_OutputService.ps1 -ArgumentList $Release,$path_OutputService
 
--- AutoDeploy.scripts/Expedia/Install_SalvatoryServices.ps1
+++ AutoDeploy.scripts/Expedia/Install_SalvatoryServices.ps1
@@ -1,4 +1,4 @@
 ﻿param([String]$server,[String]$Release)
 echo "Install Services on $server release $Release"
 
-Invoke-Command -ComputerName $server -FilePath LocalInstall_SalvatoryServices.ps1 -ArgumentList $Release
+Invoke-Command -ComputerName $server -FilePath ScriptsExpedia\LocalInstall_SalvatoryServices.ps1 -ArgumentList $Release
