630f61f81d5f070e0ee25b863cf1de06db486a71
Alexander Vykhrishchuk <angelok139@gmail.com>
2019-04-19 18:10:46 +0300

    Change path

--- AutoDeploy.scripts/Expedia/Install_HotelHub.ps1
+++ AutoDeploy.scripts/Expedia/Install_HotelHub.ps1
@@ -1,5 +1,5 @@
 ﻿param([String]$server,[String]$Release,[String]$path_HotelHubAPI)
 echo "Install API on $server release $Release to $path_HotelHub"
 
-Invoke-Command -ComputerName $server -FilePath LocalInstall_HotelHub.ps1 -ArgumentList $Release,$path_HotelHubAPI
+Invoke-Command -ComputerName $server -FilePath ScriptsExpedia\LocalInstall_HotelHub.ps1 -ArgumentList $Release,$path_HotelHubAPI
 
