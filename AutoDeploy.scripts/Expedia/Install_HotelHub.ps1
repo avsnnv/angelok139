8e790c044f86f4cae6a535497854351d12828786
Alexander Vykhrishchuk <angelok139@gmail.com>
2019-04-19 18:06:17 +0300

    Add Install hotelHub

---
+++ AutoDeploy.scripts/Expedia/Install_HotelHub.ps1
@@ -0,0 +1,5 @@
+﻿param([String]$server,[String]$Release,[String]$path_HotelHubAPI)
+echo "Install API on $server release $Release to $path_HotelHub"
+
+Invoke-Command -ComputerName $server -FilePath LocalInstall_HotelHub.ps1 -ArgumentList $Release,$path_HotelHubAPI
+
---
+++ AutoDeploy.scripts/Expedia/LocalInstall_HotelHub.ps1
@@ -0,0 +1,7 @@
+﻿param([String]$Release,[String]$path_HotelHubAPI)
+
+cd c:\temp
+
+mkdir $path_ExpediaAPI
+robocopy CBooking.MetaHotelHub.API $path_ExpediaAPI /E 
+
